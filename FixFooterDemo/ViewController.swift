2fae07bc386a2e1c239a35a4084209250cae4497
jisulee154@gmail.com <127957826+jisulee154@users.noreply.github.com>
2023-09-09 19:42:42 +0900

    first commit

---
+++ FixFooterDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FixFooterDemo
+//
+//  Created by 이지수 on 2023/09/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
