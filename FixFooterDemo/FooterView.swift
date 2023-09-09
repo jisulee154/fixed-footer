8438ee26651053331928716b84f884cbefbbb8f6
jisulee154@gmail.com <127957826+jisulee154@users.noreply.github.com>
2023-09-09 21:16:45 +0900

    add view, label

---
+++ FixFooterDemo/FooterView.swift
@@ -0,0 +1,48 @@
+//
+//  FooterView.swift
+//  FixFooterDemo
+//
+//  Created by 이지수 on 2023/09/09.
+//
+
+import UIKit
+import SnapKit
+
+class FooterView: UIView {
+    var containerView: UIView!
+    var label: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUpView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented.")
+    }
+    
+    func setUpView() {
+        containerView = UIView()
+        label = UILabel()
+        label.text = "It's a fixed footer"
+        
+        self.addSubview(containerView)
+        self.addSubview(label)
+        
+        //MARK: - setting auto layout
+        containerView.snp.makeConstraints {
+            $0.edges.equalTo(self)
+        }
+        
+        label.snp.makeConstraints {
+            $0.centerX.equalTo(self)
+            $0.centerY.equalTo(self)
+            $0.width.equalTo(200)
+            $0.height.equalTo(80)
+        }
+        
+        
+    
+    }
+}
